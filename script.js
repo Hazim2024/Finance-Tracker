96a910ff9a3b99372edb5e65131cf7e7770633bc
Hazim2024 <hazimiftikhar@gmail.com>
2024-08-08 15:19:54 +0300

    new update

--- script.js
+++ script.js
@@ -5,6 +5,26 @@ document.addEventListener('DOMContentLoaded', () => {
     const submitButton = document.querySelector('.btn-submit-expense');
     const expenseList = document.querySelector('.expense-list');
     const budgetElement = document.querySelector('#month-budget');
+
+    const wrapper=document.querySelector('.wrapper');
+    const loginLink=document.querySelector('.login-link');
+    const registerLink=document.querySelector('.register-link');
+    const btnPopup = document.querySelector('.btnLogin-popup');
+    const iconClose = document.querySelector('.icon-close');
+
+    registerLink.addEventListener('click',()=>{
+        wrapper.classList.add('active');
+    });
+    loginLink.addEventListener('click',()=>{
+        wrapper.classList.remove('active');
+    });
+    btnPopup.addEventListener('click',()=>{
+        wrapper.classList.add('active-popup');
+    });
+    iconClose.addEventListener('click',()=>{
+        wrapper.classList.remove('active-popup');
+    });
+
     let currentType = 'Savings';
     let totalBudget = 0;
 
